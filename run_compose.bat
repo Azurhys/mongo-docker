1e6df217a9dce9260a47d8052b80a28c377a5bbf
Azurhys <123641752+Azurhys@users.noreply.github.com>
2023-09-15 11:58:09 +0200

    modif script

--- run_compose.bat
+++ run_compose.bat
@@ -1,3 +1,14 @@
-@echo off
+@echo OFF
+set OPTION=%1
 
-docker compose -f mongo-compose.yml up -d
+if %OPTION%. == start. GOTO START
+if %OPTION%. == stop. GOTO STOP
+@echo run_compose [start|stop]
+
+:START
+docker-compose -f mongo-compose.yml up -d
+goto :eof
+
+:STOP
+docker-compose -f mongo-compose.yml down
+goto :eof
